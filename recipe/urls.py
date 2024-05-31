c48e6ebcd45880d37cc670db98e4d138f7378c60
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-05-31 16:21:30 +0300

    Fixed bugs

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('main/', views.main, name='main'),
     path('categories/', views.category_list, name='category_list'),
     path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
