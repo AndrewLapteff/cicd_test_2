adadc285b359d805a090c52c51e7e463bd86cde8
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-05-31 15:32:29 +0300

    Added view for recipe's details

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('main/', views.main, name='main'),
     path('categories/', views.category_list, name='category_list'),
+    path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from recipe.models import Category, Recipe
 
@@ -11,3 +11,7 @@ def main(request):
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'category_list.html', {'categories': categories})
+
+def recipe_detail(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
